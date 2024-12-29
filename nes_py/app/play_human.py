23a290fd8caf58f7d9d93190ffd1b0ccb05886da
rickyegl <135090025+rickyegl@users.noreply.github.com>
2024-12-28 19:09:11 -0600

    Update play_human.py

--- nes_py/app/play_human.py
+++ nes_py/app/play_human.py
@@ -36,18 +36,13 @@ def play_human(env: gymnasium.Env, callback=None):
     else:
         raise ValueError('env has no get_keys_to_action method')
     # create the image viewer
-    try:
-        viewer = ImageViewer(
-            env.spec.id if env.spec is not None else env.__class__.__name__,
-            env.observation_space.shape[0], # height
-            env.observation_space.shape[1], # width
-            monitor_keyboard=True,
-            relevant_keys=set(sum(map(list, keys_to_action.keys()), []))
-        )
-    except:
-        headless = True
-        viewer = None
-        print("No Writeable Display Found")
+    viewer = ImageViewer(
+        env.spec.id if env.spec is not None else env.__class__.__name__,
+        env.observation_space.shape[0], # height
+        env.observation_space.shape[1], # width
+        monitor_keyboard=True,
+        relevant_keys=set(sum(map(list, keys_to_action.keys()), []))
+    )
     # create a done flag for the environment
     done = True
     # prepare frame rate limiting
@@ -68,27 +63,22 @@ def play_human(env: gymnasium.Env, callback=None):
             if done:
                 done = False
                 state = env.reset()
-                if not headless:
-                    viewer.show(env.unwrapped.screen)
-            # unwrap the action based on pressed relevant keys
-            if not headless:
-                action = keys_to_action.get(viewer.pressed_keys, _NOP)
-                next_state, reward, done, _ = env.step(action)
-            if not headless:
                 viewer.show(env.unwrapped.screen)
+            # unwrap the action based on pressed relevant keys
+            action = keys_to_action.get(viewer.pressed_keys, _NOP)
+            next_state, reward, done, _ = env.step(action)
+            viewer.show(env.unwrapped.screen)
             # pass the observation data through the callback
             if callback is not None:
                 callback(state, action, reward, done, next_state)
             state = next_state
             # shutdown if the escape key is pressed
-            if not headless:
-                if viewer.is_escape_pressed:
-                    break
+            if viewer.is_escape_pressed:
+                break
     except KeyboardInterrupt:
         pass
 
-    if not headless:
-        viewer.close()
+    viewer.close()
     env.close()
 
 
