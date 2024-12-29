c59f74f2fe53f15420b95724f4135c653de56a89
rickyegl <135090025+rickyegl@users.noreply.github.com>
2024-12-28 19:03:33 -0600

    Update play_human.py

--- nes_py/app/play_human.py
+++ nes_py/app/play_human.py
@@ -68,26 +68,26 @@ def play_human(env: gymnasium.Env, callback=None):
             if done:
                 done = False
                 state = env.reset()
-                if !headless:
+                if not headless:
                     viewer.show(env.unwrapped.screen)
             # unwrap the action based on pressed relevant keys
-            if !headless:
+            if not headless:
                 action = keys_to_action.get(viewer.pressed_keys, _NOP)
                 next_state, reward, done, _ = env.step(action)
-            if !headless:
+            if not headless:
                 viewer.show(env.unwrapped.screen)
             # pass the observation data through the callback
             if callback is not None:
                 callback(state, action, reward, done, next_state)
             state = next_state
             # shutdown if the escape key is pressed
-            if !headless:
+            if not headless:
                 if viewer.is_escape_pressed:
                     break
     except KeyboardInterrupt:
         pass
 
-    if !headless:
+    if not headless:
         viewer.close()
     env.close()
 
